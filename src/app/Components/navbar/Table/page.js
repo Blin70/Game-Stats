ce17197642dc24a88b3e2d568939bfb4ec2220f2
Blin Axhirexha <blini.axhirexha@gmail.com>
2024-06-26 05:19:40 +0200

    Completed Table

--- src/app/Components/navbar/Table/page.js
+++ src/app/Components/navbar/Table/page.js
@@ -1,7 +1,43 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import Navbar from "../Navbar";
+import TableForm from "./TableForm";
+import { FetchTableData } from "./TableDataHandler";
+
 const Table = () => {
+    const [tableData, setTableData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await FetchTableData();
+            setTableData(data);
+        };
+
+        fetchData();
+    }, []);
+
     return (
-        <h1>Test</h1>
+        <div className="h-screen">
+            <Navbar />
+            <TableForm />
+            <table className="bg-[#d5d7d8] w-2/5 mx-auto text-center">
+                <thead>
+                    <tr className='h-20 text-xl'>
+                        <th className="w-1/12">Id</th>
+                        <th className="w-3/12">Username</th>
+                        <th className="w-3/12">Email</th>
+                        <th className="w-3/12">Phone</th>
+                        <th className="w-2/12">Joined</th>
+                    </tr>
+                </thead>
+                    <tbody>
+                        {tableData}
+                   </tbody>
+            </table>
+            
+        </div>
     );
-}
+};
 
 export default Table;
