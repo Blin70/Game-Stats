02bade4b5f4f0ebaf6a305f84390d907a000e982
Blin Axhirexha <blini.axhirexha@gmail.com>
2024-07-01 02:13:42 +0200

    adding signin/signup page + fexes

--- src/app/(table)/loading.js
+++ src/app/(table)/loading.js
@@ -1,6 +1,8 @@
 const Loading = () => {
     return (
-        <h1 className="absolute top-1/2 left-1/2">Loading...</h1>
+        <div className="h-screen w-screen bg-white" >
+            <h1 className="absolute top-1/2 left-1/2 text-black">Loading...</h1>
+        </div>
     );
 }
 
