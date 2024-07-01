7d523467bbd2ea77b730072e17e4adaf0dc7a09c
Blin Axhirexha <blini.axhirexha@gmail.com>
2024-07-01 20:12:55 +0200

    setup account related routes

--- src/app/(auth)/SignIn/page.js
+++ src/app/(auth)/SignIn/page.js
@@ -1,13 +1,20 @@
+import Link from "next/link";
+import AuthInput from "@/app/Components/auth/AuthInput";
+
 const SignIn = () => {
 
     return (
         <div className="absolute h-1/2 w-1/3 inset-0 m-auto">
             <form>
                 <h1 className="m-6 text-center text-5xl">Sign In</h1>
-                <input type="text" name="Email" placeholder="Email" className="block h-14 w-4/6 rounded-2xl mx-auto my-4 text-2xl p-2" required/>
-                <input type="text" name="Password" placeholder="Password" className="block h-14 w-4/6 rounded-2xl mx-auto my-4 text-2xl p-2" required/>
-                <button type="submit" className="block h-14 w-4/6 rounded-2xl mx-auto my-4 text-3xl p-2 bg-[#7f7f7f]">SignIn</button>
+                <AuthInput type='text' name='Email' />
+                <AuthInput type='password' name='Password' />
+                <button type="submit" className="block h-14 w-4/6 rounded-2xl mx-auto mt-6 text-3xl p-2 bg-[#7f7f7f]">SignIn</button>
             </form>
+            <div className="text-lg mx-auto h-fit w-fit block mt-2 space-x-2">
+                <label>Dont have an account?</label>
+                <Link href='/SignUp'>SignUp</Link>
+            </div>
         </div>
     );
 }
--- src/app/(auth)/SignUp/page.js
+++ src/app/(auth)/SignUp/page.js
@@ -1,14 +1,21 @@
+import Link from "next/link";
+import AuthInput from "@/app/Components/auth/AuthInput";
+
 const SignUp = () => {
 
     return (
         <div className="absolute h-1/2 w-1/3 inset-0 m-auto">
             <form>
                 <h1 className="m-6 text-center text-5xl">Sign Up</h1>
-                <input type="text" name="Username" placeholder="Username" className="block h-14 w-4/6 rounded-2xl mx-auto my-4 text-2xl p-2" required/>
-                <input type="text" name="Email" placeholder="Email" className="block h-14 w-4/6 rounded-2xl mx-auto my-4 text-2xl p-2" required/>
-                <input type="text" name="Password" placeholder="Password" className="block h-14 w-4/6 rounded-2xl mx-auto my-4 text-2xl p-2" required/>
-                <button type="submit" className="block h-14 w-4/6 rounded-2xl mx-auto my-4 text-3xl p-2 bg-[#7f7f7f]">SignUp</button>
+                <AuthInput type='text' name='Username' />
+                <AuthInput type='text' name='Email' />
+                <AuthInput type='password' name='Password' />
+                <button type="submit" className="block h-14 w-4/6 rounded-2xl mx-auto mt-6 text-3xl p-2 bg-[#7f7f7f]">SignUp</button>
             </form>
+            <div className="text-lg mx-auto h-fit w-fit block mt-2 space-x-2">
+                <label>Already have an account?</label>
+                <Link href='/SignIn'>SignIn</Link>
+            </div>
         </div>
     );
 }
