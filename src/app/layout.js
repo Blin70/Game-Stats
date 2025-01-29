1503a0108271f9de02ff15d955a5324b58003b80
Blin Axhirexha <blini.axhirexha@gmail.com>
2025-01-29 17:26:26 +0100

    Set up Toast from shadcn

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Toaster } from "@/components/ui/toaster"
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <Toaster />
       </body>
     </html>
   );
