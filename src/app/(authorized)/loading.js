7c0605d4987e7660abcf9a41c7043fdb40cacbf0
Blin Axhirexha <blini.axhirexha@gmail.com>
2025-04-15 17:10:42 +0200

    Removed loading.js in /(authorized). Added new loading.js file that displays skeletons for /user/Profile

--- src/app/(authorized)/loading.js
+++
@@ -1,7 +0,0 @@
-const loading = () => {
-    return(
-        <h1 className="text-7xl h-fit w-fit mx-auto content-center">Loading... from authorized layout</h1>
-    );
-}
-
-export default loading;
